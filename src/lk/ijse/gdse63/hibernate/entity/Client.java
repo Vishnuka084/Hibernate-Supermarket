55691942661880cc794d7a0c87332e5aa06fa56b
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-03-14 11:55:32 +0530

    add attribute

--- src/lk/ijse/gdse63/hibernate/entity/Client.java
+++ src/lk/ijse/gdse63/hibernate/entity/Client.java
@@ -1,25 +1,21 @@
 package lk.ijse.gdse63.hibernate.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@ToString
 @Entity(name = "client")
+@ToString
 @Table(name = "client")
-public class Vehicle {
+public class Client {
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id ;
     private String name;
-
 }
--- src/lk/ijse/gdse63/hibernate/entity/Clinet.java
+++
@@ -1,5 +0,0 @@
-package lk.ijse.gdse63.hibernate.entity;
-
-
-public class Clinet {
-}
